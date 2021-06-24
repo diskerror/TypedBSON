6437c6d230e16c8f0366b08a5ac7ef6790ce15ff
Reid Woodbury Jr <reid@rawsound.com>
2021-06-23 18:11:40 -0700

    Removed Twitter code from DateTrait.

--- src/DateTrait.php
+++ src/DateTrait.php
@@ -4,17 +4,10 @@
 namespace Diskerror\TypedBSON;
 
 
-use DateTime;
-use DateTimeZone;
 use MongoDB\BSON\UTCDateTimeInterface;
-use function in_array;
-use function substr;
-use const STDERR;
 
 trait DateTrait
 {
-	protected static $_days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
 	/**
 	 * Adds the ability to accept a Mongo\BSON\UTCDateTime object.
 	 *
@@ -25,10 +18,5 @@ trait DateTrait
 		if (is_object($time) && $time instanceof UTCDateTimeInterface) {
 			$time = $time->toDateTime();
 		}
-//
-//		if (is_string($time) && in_array(substr($time, 0, 3), self::$_days)) {
-////					Twitter:	Thu Jun 10 05:24:16 + 0000 2021
-//			$time = DateTime::createFromFormat('D m d H:m:s + Z Y', $time, $timezone);
-//		}
 	}
 }
